4055e2df3e6942efa5c3243790fe1350747618c3
Nguyen Duy Y <duyy.uit@gmail.com>
2019-06-27 01:01:03 +0700

    not support ES6 yet

--- src/server/environments/firestore_factory.js
+++ src/server/environments/firestore_factory.js
@@ -2,12 +2,17 @@ const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
 module.exports = () => {
-    /*if (env === "dev") {
-        //TODO :connect with emulator
-    } else if (env === "prod") {
-        //TODO : connect to firestore
-    }*/
+    if (this.firestore) {
+        return this.firestore;
+    } else {
+        /*if (env === "dev") {
+            //TODO :connect with emulator
+        } else if (env === "prod") {
+            //TODO : connect to firestore
+        }*/
 
-    admin.initializeApp(functions.config().firebase);
-    return admin.firestore();
+        admin.initializeApp(functions.config().firebase);
+        this.firestore = admin.firestore();
+    }
+
 };
--- src/server/models/restaurant.js
+++ src/server/models/restaurant.js
@@ -1,76 +1,72 @@
-import AbstractModel from "./AbstractModel";
-import firebaseAdmin from "firebase-admin";
-class Restaurant extends AbstractModel {
-    constructor(input, id) {
-        super(input);
-        this._name = input.name || null;
-        this._creator = input.creator || null;
-        this._address = input.address || null;
-        this._location = input.location || null;
-        if (!this._location && !!input.lat && !!input.lng) {
-            this.location(input.lat, input.lng);
-        }
-        this._type = input.type || "RESTAURANT";
-
-        if (id) {
-            this._id = id;
-        }
+function Restaurant(input, id) {
+    this._name = input.name || null;
+    this._creator = input.creator || null;
+    this._address = input.address || null;
+    this._location = input.location || null;
+    this._lat = input.lat || null;
+    this._lng = input.lng || null;
+    this._type = input.type || "RESTAURANT";
+    if (id) {
+        this._id = id;
     }
-
-    name(value) {
-        if (value) {
-            this._name = value;
-        }
-        return this._name;
-
-    }
-
-    address(value) {
-        if (value) {
-            this._address = value;
-        }
-        return this._address;
-
-    }
-
-
-    creator(value) {
-        if (value) {
-            this._creator = value;
-        }
-        return this._creator;
-
-    }
-
-    location(lat, lng) {
-        if (!!lat && !!lng) {
-            const point = new firebaseAdmin.firestore.GeoPoint(lat, lng);
-            this._location = point;
-        }
-        return this._location;
-    }
-
-    type(value) {
-        if (value) {
-            this._type = value;
-        }
-        return this._type;
-    }
-
-    toJSON() {
-        return {
-            id : this._id,
-            name : this._name,
-            creator : this._creator,
-            address : this._address,
-            location : this._location
-        }
-    }
-
-    collectionName() {
-        return "restaurants";
-    }
-
 }
 
-export default Restaurant;
+Restaurant.prototype.name = (value) => {
+    if (value) {
+        this._name = value;
+    }
+    return this._name;
+
+};
+
+Restaurant.prototype.address = (value) => {
+    if (value) {
+        this._address = value;
+    }
+    return this._address;
+
+};
+
+
+Restaurant.prototype.creator = (value) => {
+    if (value) {
+        this._creator = value;
+    }
+    return this._creator;
+
+};
+
+Restaurant.prototype.location = (lat, lng) => {
+    if (!!lat && !!lng) {
+        this._lat = lat;
+        this._lng = lng;
+    }
+    return {
+        lat : this._lat,
+        lng : this._lng
+    }
+};
+
+Restaurant.prototype.type = (value) => {
+    if (value) {
+        this._type = value;
+    }
+    return this._type;
+};
+
+Restaurant.prototype.toJSON = () => {
+    return {
+        id : this._id,
+        name : this._name,
+        creator : this._creator,
+        address : this._address,
+        lat : this._lat,
+        lng: this._lng
+    }
+};
+
+Restaurant.prototype.collectionName = () => {
+    return "restaurants";
+};
+
+module.exports = Restaurant;

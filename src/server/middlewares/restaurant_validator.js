a553568fa7d2f5392623156aeb8d4bc09849e55e
nguyenduyy <duyy.uit@gmail.com>
2019-06-29 16:31:06 +0700

    correct logic

--- src/server/middlewares/restaurant_validator.js
+++ src/server/middlewares/restaurant_validator.js
@@ -6,12 +6,12 @@ let ErrorCodes = require("../utils/error_codes");
 module.exports = (req, res, next) => {
     if (req.body) {
         let {name, address, category, type, lat, lng} = req.body;
-        let valid = name !== null && validator.isAlphanumeric(name);
-        valid &= address !== null && validator.isAlphanumeric(address);
-        valid &= category !== null && Category.hasOwnProperty(category);
-        valid &= type !== null && Type.hasOwnProperty(type);
-        valid &= lat !== null && validator.isNumeric(lat);
-        valid &= lng !== null && validator.isNumeric(lng);
+        let valid = !name  || validator.isAlphanumeric(name);
+        valid &= !address  || validator.isAlphanumeric(address);
+        valid &= !category  || Category.hasOwnProperty(category);
+        valid &= !type  || Type.hasOwnProperty(type);
+        valid &= !lat  || validator.isNumeric(lat);
+        valid &= !lng  || validator.isNumeric(lng);
         if (valid) next();
         else {
             ErrorHandler.error(res, ErrorCodes.WRONG_FORMAT, "")

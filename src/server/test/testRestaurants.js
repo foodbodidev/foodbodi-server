3e7cafa9dc61ef5d748f8bd3c12ec658d9b32a56
nguyenduyy <duyy.uit@gmail.com>
2019-06-29 13:55:55 +0700

    init unit test

--- src/server/test/testRestaurants.js
+++ src/server/test/testRestaurants.js
@@ -1,5 +1,24 @@
+var describe = require ('mocha').describe;
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var assert = chai.assert;
 let Restaurant = require("../models/restaurant");
 
-let r = new Restaurant({name : "A"}, "1213");
+chai.use(chaiHttp);
 
-console.log(r.toJSON());
+describe('test Restaurant', function() {
+    it("test model", (done) => {
+        let r = new Restaurant({name : "A"}, "1213");
+        const json = r.toJSON();
+        assert.equal(json.name, "A");
+        assert.equal(json.id, "1213");
+        done();
+    });
+
+    it("test create restaurant", (done) => {
+        let r = new Restaurant({name : "Test restauranr"});
+
+        done();
+    })
+
+});

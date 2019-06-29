a2e211e89f57d796a8444fb13e8781a9a51ce806
nguyenduyy <duyy.uit@gmail.com>
2019-06-29 13:55:24 +0700

    dev env

--- src/server/environments/firestore_factory.js
+++ src/server/environments/firestore_factory.js
@@ -1,18 +1,23 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+let serviceAccount = require('../credentials/firestore-service-account.json');
 
 module.exports = () => {
     if (this.firestore) {
         return this.firestore;
     } else {
-        /*if (env === "dev") {
-            //TODO :connect with emulator
+        const env = process.env.FOODBODI_ENV || "dev";
+        if (env === "dev") {
+            admin.initializeApp({
+                credential: admin.credential.cert(serviceAccount)
+            });
+            this.firestore = admin.firestore();
         } else if (env === "prod") {
-            //TODO : connect to firestore
-        }*/
-
-        admin.initializeApp(functions.config().firebase);
-        this.firestore = admin.firestore();
+            admin.initializeApp(functions.config().firebase);
+            this.firestore = admin.firestore();
+        } else if (env === "test") {
+            //TODO : mock firestore
+        }
     }
 
 };

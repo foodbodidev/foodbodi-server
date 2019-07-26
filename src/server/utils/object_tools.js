8da32fa4a1831fa2cb0acf28d6481a2954263b3b
nguyenduyy <duyy.uit@gmail.com>
2019-07-26 20:10:54 +0700

    m

--- src/server/utils/object_tools.js
+++ src/server/utils/object_tools.js
@@ -1,6 +1,6 @@
 exports.clean = (obj) => {
     for (var propName in obj) {
-        if (obj[propName] === null || obj[propName] === undefined) {
+        if (obj[propName] === null || typeof obj[propName] === "undefined") {
             delete obj[propName];
         }
     }
